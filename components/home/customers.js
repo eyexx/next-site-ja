a3acd151138c11674c1ebd3a293998b77f2ccdba
Kosukey <koshuke0217@icloud.com>
2018-12-02 19:52:35 +0900

    index pageの翻訳

--- components/home/customers.js
+++ components/home/customers.js
@@ -1,13 +1,19 @@
-import Container from '../container'
-import Button from '../button'
-import SectionHeader from '../section-header'
-import ShowcasePreview from './showcase-preview'
+import Container from '../container';
+import Button from '../button';
+import SectionHeader from '../section-header';
+import ShowcasePreview from './showcase-preview';
 
-import Preview from '../blog/preview'
+import Preview from '../blog/preview';
 
-export default () => <Container gray wide center padding role='region' aria-labelledby='customers'>
-  <SectionHeader anchor="showcases" id="customers" title='Who’s Using Next.js' description='We’re honored some of the most talented creatives out there build with Next.js'/>
-  {/*
+export default () => (
+  <Container gray wide center padding role="region" aria-labelledby="customers">
+    <SectionHeader
+      anchor="showcases"
+      id="customers"
+      title="Next.jsを使っている企業"
+      description="We’re honored some of the most talented creatives out there build with Next.js"
+    />
+    {/*
   <Container center>
     <div>
       <style jsx>{`
@@ -28,15 +34,18 @@ export default () => <Container gray wide center padding role='region' aria-labe
     <h3 className="f-reset subtitle fw4">Meet hundreds of beautiful websites <br className="display-mobile"/>powered by Next.js</h3>
   </div>
   */}
-  <ShowcasePreview/>
-  <div>
-    <style jsx>{`
-      div {
-        z-index: 1;
-        position: relative;
-        margin-top: 2rem;
-      }
-    `}</style>
-    <Button href="/showcase" invert>View Gallery</Button>
-  </div>
-</Container>
+    <ShowcasePreview />
+    <div>
+      <style jsx>{`
+        div {
+          z-index: 1;
+          position: relative;
+          margin-top: 2rem;
+        }
+      `}</style>
+      <Button href="/showcase" invert>
+        ギャラリーを見る
+      </Button>
+    </div>
+  </Container>
+);
